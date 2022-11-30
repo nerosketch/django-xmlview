abdaa2b99cd2e3363164cddaa5f7150f46dbccaa
Дмитрий <nerosketch@gmail.com>
2022-11-30 13:23:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+    ],
+    packages=['xmlview'],
 )
